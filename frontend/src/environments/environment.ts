96270e894d5201c8d9aca2b7184aa841ccaa33c3
abik6 <pabloroelas01@gmail.com>
2026-04-05 17:06:34 +0200

    config: environment.ts apunta a localhost para develop

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    production: true,
-    // ⚠️ Cambia esta URL por la dirección real de tu API en el servidor de producción
-    // Ejemplos: 'https://api.cecolivenc.es' o 'https://cecolivenc.es/api'
-    apiUrl: 'https://api.cecolivenc.es'
+    production: false,
+    apiUrl: 'http://localhost:3000'
 };
